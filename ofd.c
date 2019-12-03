cf1d9a11905c52a873a0a550f8a80ec3e4b7e893
Lokesh Jaliminche <lokesh.jaliminche@gmail.com>
2019-12-02 17:47:56 -0800

    changes

--- ofd.c
+++ ofd.c
@@ -6,19 +6,19 @@
 
 static int __init ofd_init(void) /* Constructor */
 {
-    printk(KERN_INFO "Namaskar: some changes");
+    printk(KERN_INFO "Hello: Registered");
 
     return 0;
 }
 
 static void __exit ofd_exit(void) /* Destructor */
 {
-    printk(KERN_INFO "Alvida: ofd unregistered");
+    printk(KERN_INFO "Bye: Unregistered");
 }
 
 module_init(ofd_init);
 module_exit(ofd_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Anil Kumar Pugalia <email@sarika-pugs.com>");
+MODULE_AUTHOR("Lokesh Jaliminche <lokesh.jaliminche@gmail.com>");
 MODULE_DESCRIPTION("Our First Driver");
